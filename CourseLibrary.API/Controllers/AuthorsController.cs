f52117df1a351e3435e30fcf9fbb0f427885973d
Radu-Alexandru Pirciu <alexandru.pirciu@fortech.ro>
2021-07-28 16:39:43 +0300

    Add functionality for getting authors

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,36 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorFromRepo);
+        }
+    }
+}
--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -2,6 +2,7 @@ using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +22,10 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
 
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
